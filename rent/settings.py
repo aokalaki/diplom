70d7c5ba707fa4c6b6a1c868299c412192f48da5
Amaly <amali.00@mail.ru>
2022-06-22 14:57:41 +0300

    Added files and changes required for deployment to heroku

--- rent/settings.py
+++ rent/settings.py
@@ -21,11 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '*w=!+hfsb27ky0ocmze&g1bw!l#5xp)x_4iw)h*)m&3wsv7*nx'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+#DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-
 
 ALLOWED_HOSTS = []
 
